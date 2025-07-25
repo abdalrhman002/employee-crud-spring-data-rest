9d0f301639ef472288847a3ecab11f27b97b1e0f
abdalrhman <abdalrhmanmogahed2002@gmail.com>
2025-07-25 03:57:21 +0300

    Remane Packge dao to repository

--- src/main/java/com/mujahid/employee_crud_api/repository/EmployeeRepository.java
+++ src/main/java/com/mujahid/employee_crud_api/repository/EmployeeRepository.java
@@ -1,8 +1,7 @@
-package com.mujahid.employee_crud_api.dao;
+package com.mujahid.employee_crud_api.repository;
 
 import com.mujahid.employee_crud_api.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
